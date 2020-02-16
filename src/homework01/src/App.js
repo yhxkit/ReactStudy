337e53e523a4a329c22b5152dca95732011e5836
yhxkit <yhxkit@gmail.com>
2020-02-16 20:03:43 +0900

    과제 추가

---
+++ src/homework01/src/App.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  state = {
+    username: "",
+    password: "",
+    disabled: true
+  };
+
+  usernameFocus = React.createRef();
+
+  handleChange = e => {
+    const { name, value } = e.target;
+
+    const update = {};
+    update[name] = value;
+
+    this.setState({
+      [name]: value
+    });
+  };
+
+  isSafe = (username, password) => {
+    if (password.length > 4 && !password.includes(username)) {
+      this.setState({
+        disabled: false
+      });
+    }
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    alert("로그인");
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            name="username"
+            value={this.state.username}
+            onChange={this.handleChange}
+            ref={this.usernameFocus}
+          ></input>
+          <input
+            name="password"
+            type="password"
+            value={this.state.password}
+            onChange={e => {
+              this.handleChange(e);
+              this.isSafe(this.state.username, this.state.password);
+            }}
+          ></input>
+          <button type="submit" disabled={this.state.disabled}>
+            Login
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default App;
